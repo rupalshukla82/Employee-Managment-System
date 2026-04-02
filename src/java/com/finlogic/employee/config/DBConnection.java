b28df3d3840fefd1f1055b643439788ba5199d14
Rupal shukla <156820534+rupalshukla82@users.noreply.github.com>
2026-04-02 11:32:19 +0530

    Dbconnection change

--- src/java/com/finlogic/employee/config/DBConnection.java
+++ src/java/com/finlogic/employee/config/DBConnection.java
@@ -2,9 +2,9 @@
 // * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 // * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
 // */
-//package com.finlogic.employee.config;
-//import java.sql.*;
-//import java.util.*;
+package com.finlogic.employee.config;
+import java.sql.*;
+import java.util.*;
 ///**
 // *
 // * @author abhi
