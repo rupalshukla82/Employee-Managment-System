0f430b23738222854b5a8aae97985a4c3f36c11a
Rupal shukla <156820534+rupalshukla82@users.noreply.github.com>
2026-04-02 22:38:28 +0530

    connection imporvment

--- src/java/com/finlogic/employee/config/DBConnection.java
+++ src/java/com/finlogic/employee/config/DBConnection.java
@@ -37,23 +37,34 @@ import java.util.*;
 //
 //}
 
+
 public class DBConnection {
 
     public static Connection getConnection() {
         Connection conn = null;
 
         try {
-            String url = System.getenv("DB_URL");
-            String user = System.getenv("DB_USER");
-            String password = System.getenv("DB_PASS");
+            // Read database info from Railway environment variables
+            String dbHost = System.getenv("DB_HOST");
+            String dbPort = System.getenv("DB_PORT");
+            String dbName = System.getenv("DB_NAME");
+            String dbUser = System.getenv("DB_USER");
+            String dbPass = System.getenv("DB_PASS");
 
+            // Build JDBC URL dynamically
+            String url = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName + "?sslMode=VERIFY_IDENTITY";
+
+            // Load MySQL driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url, user, password);
 
-            System.out.println("DBConnection done");
+            // Connect to DB
+            conn = DriverManager.getConnection(url, dbUser, dbPass);
+
+            System.out.println("DBConnection successful!");
 
         } catch (Exception e) {
-            System.out.println("ERROR IS:" + e);
+            System.out.println("ERROR in DBConnection: " + e);
+            e.printStackTrace();
         }
 
         return conn;
