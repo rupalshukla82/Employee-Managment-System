ab254fd31cfa72c771d2d0e1714964d2f082968e
Rupal shukla <156820534+rupalshukla82@users.noreply.github.com>
2026-04-03 20:55:53 +0530

    update the start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
+PORT=${PORT:-8080}
+
 echo "Using PORT: $PORT"
 
-# Replace default port 8080 with Railway dynamic port
 sed -i "s/port=\"8080\"/port=\"${PORT}\"/" /usr/local/tomcat/conf/server.xml
 
-# Start Tomcat
 catalina.sh run
